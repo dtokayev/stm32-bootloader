9a6866db2cd5be656a31af862ec3579e14090ada
Dmytro Tokayev <60584648+dtokayev@users.noreply.github.com>
2025-09-20 18:24:05 +0200

    simplify jump_to_main function

--- bootloader/src/bootloader.c
+++ bootloader/src/bootloader.c
@@ -25,13 +25,8 @@ static void gpio_setup(void) {
 }
 
 static void jump_to_main(void) {
-  typedef void (*void_fn)(void);
-
-  uint32_t* reset_vector_entry = (uint32_t*)(MAIN_APP_START_ADDRESS + 4U);
-  uint32_t* reset_vector = (uint32_t*)(*reset_vector_entry);
-  void_fn jump_fn = (void_fn)reset_vector;
-
-  jump_fn();
+  vector_table_t* main_vector_table = (vector_table_t*)MAIN_APP_START_ADDRESS;
+  main_vector_table->reset();
 }
 
 int main(void) {
@@ -41,13 +36,9 @@ int main(void) {
   comms_setup();
 
   simple_timer_t timer;
-  simple_timer_setup(&timer, 1000, true);
 
   while (true) {
-    if (simple_timer_has_elapsed(&timer)) {
-      volatile int x = 0;
-      x++;
-    }
+
   }
 
   // TODO: Teardown
